7c3bf4dc6761d37e2999c2bff8a6529bcd1dd1f7
Ogreman <theogreman@gmail.com>
2014-03-11 15:56:20 +0000

    logs

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # app.py
 
-import datetime, os
+from datetime import timedelta, datetime
+import requests
+import os
 
 from flask import request, url_for, render_template
 
@@ -10,14 +12,40 @@ from flask.ext.api.renderers import HTMLRenderer
 from flask.ext.api.exceptions import APIException
 from flask.ext.sqlalchemy import SQLAlchemy
 
-from sqlalchemy import Column, String, Integer
-from sqlalchemy.ext.mutable import MutableDict
-from sqlalchemy.dialects.postgresql import HSTORE
+from sqlalchemy import Column, String, Integer, DateTime
 
+from celery import Celery
+from celery.task import periodic_task
 
 app = FlaskAPI(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config.update(
+    SQLALCHEMY_DATABASE_URI=os.environ['DATABASE_URL'],
+)
 db = SQLAlchemy(app)
+
+REDIS_URL = os.environ.get('REDISTOGO_URL', 'redis://localhost')
+celery = Celery('tasks', broker=REDIS_URL)
+
+APPS_TO_PING = [
+    "http://isitaleapyear.herokuapp.com/",
+]
+
+
+def ping_url(url):
+    resp = requests.get(url)
+    return resp.status_code
+
+
+@periodic_task(run_every=timedelta(minutes=1))
+def log_ping():
+    for url in APPS_TO_PING:
+        log = TaskHistory(
+            title="{action} {url}".format(action="log_ping", url=url),
+            result=ping_url(url),
+            time=datetime.now(),
+        )
+        db.session.add(log)
+    db.session.commit()
 
 
 class TaskHistory(db.Model):
@@ -25,7 +53,8 @@ class TaskHistory(db.Model):
     __tablename__ = "taskhistory"
     id = Column(Integer, primary_key=True, autoincrement=True)
     task = Column(String)
-    result = Column(MutableDict.as_mutable(HSTORE))
+    result = Column(Integer)
+    time = Column(DateTime)
 
     def __repr__(self):
         return "Task history of task: {task}.".format(
@@ -37,6 +66,7 @@ class TaskHistory(db.Model):
             'id': self.id,
             'task': self.task,
             'result': self.result,
+            'tiime': self.time,
         }
 
     @classmethod

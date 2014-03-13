5b35970de32621b9bde61c8ca8eb4d1408169044
Ogreman <theogreman@gmail.com>
2014-03-13 15:04:09 +0000

    leap

--- app.py
+++ app.py
@@ -9,12 +9,14 @@ import heroku
 
 from celery import Celery
 from celery.task import periodic_task
+from celery.schedules import crontab
 
 
 REDIS_URL = os.environ.get('REDISTOGO_URL', 'redis://localhost')
 celery = Celery(__name__, broker=REDIS_URL)
 
 task_log_url = "http://thisisatasklog.herokuapp.com/api/"
+tweet_url = "http://tweetboard.herokuapp.com/api/"
 
 
 def ping_url(url):
@@ -44,7 +46,6 @@ def log_ping():
 @periodic_task(run_every=timedelta(hours=24))
 def tweet_check():
     get_url = "http://tweetboard.herokuapp.com/api/recent/"
-    post_url = "http://tweetboard.herokuapp.com/api/"
     response = requests.get(get_url)
     if response.ok:
         posts = response.json()
@@ -52,7 +53,32 @@ def tweet_check():
             tweet = "There were {n} new anonymous tweets today.".format(
                 n=len(posts)
             )
-            requests.post(post_url, data={ 'text': tweet })
+            requests.post(tweet_url, data={'text': tweet})
+    payload = {
+        "task": sys._getframe().f_code.co_name,
+        "target": get_url,
+        "result": response.status_code,
+        "time": datetime.now(),
+    }
+    log = requests.post(
+        task_log_url,
+        data=payload
+    )
+
+
+@periodic_task(run_every=crontab(day_of_month='1', month_of_year='1'))
+def leap_tweet():
+    get_url = "http://isitaleapyear.herokuapp.com/api/?year={year}".format(
+        year=datetime.now().year
+    )
+    response = requests.get(get_url)
+    if response.ok:
+        leap = response.json()
+        tweet = "{year} {leap} a leap year this year.".format(
+            year=leap['year'],
+            leap="is" if leap['leap'] else "is not",
+        )
+        requests.post(tweet_url, data={'text': tweet})
     payload = {
         "task": sys._getframe().f_code.co_name,
         "target": get_url,
